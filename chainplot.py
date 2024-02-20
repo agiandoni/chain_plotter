de3941cdf828b311fd6616c38c6a51c3151a4fd4
agiandoni <112551814+agiandoni@users.noreply.github.com>
2024-02-20 15:23:28 +0100

    Update chainplot.py

--- chainplot.py
+++ chainplot.py
@@ -5,9 +5,11 @@ Created on Thu Feb 15 13:22:07 2024
 @author: Andoni Agirre Arabolaza
 """
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 import warnings
+import itertools
+
 
 def generate_coordinates(center_pos, angle, shape_type, connection_type, a=1):
     """
@@ -43,7 +45,16 @@ def generate_coordinates(center_pos, angle, shape_type, connection_type, a=1):
     """
     if shape_type == "hexagon" or shape_type == "hex" or shape_type == "h":
         shape_type = "h"
-        coords = [(-np.sqrt(3)*a/2, 0.5),(-np.sqrt(3)*a/2, -0.5), (0,-a), (np.sqrt(3)*a/2,-0.5), (np.sqrt(3)*a/2,0.5),(0,a)]
+        
+        coords = [
+            (-np.sqrt(3)*a/2, 0.5),
+            (-np.sqrt(3)*a/2, -0.5),
+            (0,-a),
+            (np.sqrt(3)*a/2,-0.5),
+            (np.sqrt(3)*a/2,0.5),
+            (0,a)
+            ]
+        
     elif shape_type == "pentagon" or shape_type == "pent" or shape_type == "p":
         shape_type = "p"
         
@@ -53,7 +64,7 @@ def generate_coordinates(center_pos, angle, shape_type, connection_type, a=1):
             (0.5*a, -a*np.sqrt((5+2*np.sqrt(5))/(20))),
             (0.25*a*(1+np.sqrt(5)), a*np.sqrt((5-np.sqrt(5))/(40))),
             (0, a*np.sqrt((5+np.sqrt(5))/(10)))
-        ]
+            ]
 
 
     #rotate around the origin first, and only then translate it to the right spot
@@ -79,7 +90,7 @@ def generate_coordinates(center_pos, angle, shape_type, connection_type, a=1):
     h_permutation = [0,1,5,2,4,3]
     hup_permutation = [0,1,2,3,5,4]
     hdn_permutation = [0,1,5,4,3,2]
-
+    
     pdn_permutation = [0,1,4,3,2] #NOTE THIS CHANGES WHICH ONE THE PENTAGON TIP IS, BETTER TO ADJUST SO THAT IT IS ALWAYS (0,a)!
     pup_permutation = [0,1,2,4,3]
     
@@ -94,6 +105,8 @@ def generate_coordinates(center_pos, angle, shape_type, connection_type, a=1):
         ordering = pdn_permutation
     elif connection_type == "pup":
         ordering = pup_permutation
+    elif connection_type == "straight":
+        ordering = h_permutation
     else:
         raise NameError('No valid snake order introduced!')
         #print("No valid snake order introduced")
@@ -160,8 +173,6 @@ def get_next_center_pos(previous_center, previous_angle, attach_where, type1, ty
     return(x,y)
 
 
-
-
 def generate_hp_chain(chaintype=1, reps=1, closinghex=True, extra_rot = 10, a=1):
     """
     Generates the chain. The returned lists have no generated polygons and the MPS
@@ -202,21 +213,33 @@ def generate_hp_chain(chaintype=1, reps=1, closinghex=True, extra_rot = 10, a=1)
             angles.append(0)
             connections.append("hup")
 
+    elif chaintype == 2:
+        angles = list(np.tile([0, -18, -36, -54], reps))
+        centers= [(0,0)]
+        polys = list(np.tile(["h", "p", "h", "p"], reps))
+        attach = list(np.tile(["straight", "below", "straight", "below"], reps))
+        connections = list(np.tile(["h", "pdn", "h", "pdn"], reps))
         
-
+        if closinghex:
+            polys.append("h")
+            angles.append(0)
+            connections.append("h")
+        
+        
+        
     else:
         print("Unsupported chain type")
     
     shapes = []
     mps_list = []
     
-    shape, mps = generate_coordinates(centers[0], angles[0], "h", connection_type="hup", a=1);
+    shape, mps = generate_coordinates(centers[0], angles[0], polys[0], connection_type=connections[0], a=a);
     shapes.append(shape)
     mps_list.append(mps)
     
     for i in range(len(angles)-1):
         centers.append(get_next_center_pos(centers[i], angles[i], attach[i], polys[i], polys[i+1]))
-        shape, mps = generate_coordinates(centers[i+1], angles[i+1], polys[i+1], connection_type=connections[i+1], a=1);
+        shape, mps = generate_coordinates(centers[i+1], angles[i+1], polys[i+1], connection_type=connections[i+1], a=a);
         shapes.append(shape)
         mps_list.append(mps)
         
@@ -271,15 +294,117 @@ def shapes_to_polys(shapes, kwargs={'fc':'white', 'ec':'k'}):
 
     
 
-def generate_chain(chaintype=1, reps=1, closinghex=True, extra_rot = 10, a=1, flatten=True):
+def generate_chain(chaintype=1, reps=1, closinghex=True, extra_rot = 10, a=1, flatten=True, kwargs={'fc':'white', 'ec':'k'}):
     #Generates the full ladder chain of type chaintype from scratch
     #Use ax.add_patch(poly) to add each element of polys to axis ax
     #Use ax.plot(*mps_clean) to plot the mps
     #Any observables can be plotted straightforwardly if they follow the mps order
     sh, mp = generate_hp_chain(chaintype=chaintype, reps=reps, closinghex=closinghex, extra_rot = extra_rot, a=a)
-    polys = shapes_to_polys(sh)
+    polys = shapes_to_polys(sh, kwargs=kwargs)
     mps_clean = clean_mps(mp, flatten=flatten, to_plot=True)
     return polys, mps_clean
 
 
+def get_midpoint(a, b):
+    x = 0.5*(a[0]+b[0])
+    y = 0.5*(a[1]+b[1])
+    return (x,y)
+
+def generate_edge_coordinates(mps, a):
+    midpoints = []
+    for combination in itertools.combinations(mps, 2):
+        p1 = combination[0]
+        p2 = combination[1]
+        if np.sqrt((p2[0]-p1[0])**2+(p2[1]-p1[1])**2)<(1.01*a):
+            midpoints.append(get_midpoint(p1, p2))
+    return midpoints
+
+def generate_mps_bond_coordinates(mps, site1, site2):
+    return get_midpoint(mps[site1], mps[site2])
+
+def generate_lattice_bonds(geometry):
+    #Takes something like ["h", "pup"]
+    
+    bonds_hex = [(1,2), 2, 2, 2]
+    bonds_pentdn = [(1,2), 3, 1] #down means the next hex goes below, ie, the free vertex is up!
+    bonds_pentup = [(1,3), 1, 2]
+    bonds_hexdn = [(1,2), 4, 1, 1]
+    bonds_hexup = [(1,4), 1, 1, 2]
+    bonds_close = [1] #These always close with 1, unless system is periodic, in that case we should work with a size n-1 unit cell! And end with (-(N-2), -(N-2))
+
+    
+    def symtobond(sym):
+        if sym == "h":
+            return bonds_hex
+        elif sym == "pup":
+            return bonds_pentup
+        elif sym == "pdn":
+            return bonds_pentdn
+        elif sym == "c":
+            return bonds_close  
+        elif sym == "hup":
+            return bonds_hexup  
+        elif sym == "hdn":
+            return bonds_hexdn  
+
+
+    """
+    periodic_bonds = []
+    
+
+    N_shapes = length(geometry)
+    N_sites_period =  
+    for char in geometry
+        if char == "h"
+            append!(periodic_bonds, bonds_hex)
+        else if char == "p↑"
+            append!()
+        elseif char
+
+    end
+    """
+
+    #bonds = [symtobond(sym); for sym in geometry]
+    #GENERATE (NON-PERIODIC) BONDS
+    bonds = []
+    for g in geometry:
+        print("g:", g)
+        bonds = [*bonds, *symtobond(g)]
+        print(bonds)
+    
+
+    if geometry[-1] != "c":
+        bonds = [*bonds, *bonds_close]
+    
+    #@assert length(bonds)==N_sites Not true, it will have at least one less!
+    return bonds
+
+def generate_lattice_from_bonds(bonds):
+    lat = []
+    bond_counter = 1
+    
+    def makeiterable(x):
+        if type(x) == int:
+            return list([x])
+        else:
+            return x
+    for i in range(len(bonds)):
+        for j in makeiterable(bonds[i]):
+            print("Adding bond from site i=", i, " to i+", j,"=",i+j)
+            #j tells us how many sites ahead we should connect to from i
+            #for example, for a single hexagon in ring-snake configuration we would have bonds = [(1,2),(2),(2),(2),(1)]
+            lat.append((i, i+j)) #no coordinates for now. sites_list[i]..., sites_list[i+2]..., "")
+            #println(lat[bond_counter])
+            bond_counter+=1
+            print(bond_counter)
+
+    return lat
+
+
+
+"""
+geometry = ["hup", "pdn", "hdn", "pup", "hup"]
+a = generate_lattice_bonds(geometry)
+b = generate_lattice_from_bonds(a)
 #Use this by calling chain() and then ax.add_patch(polys[:]).
+"""
